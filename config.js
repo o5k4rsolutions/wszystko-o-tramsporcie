4cf298082064e40f28844cf184a78e3229b77cd3
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-08-27 11:15:16 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const part1 = 'xkeysib-d3f0482eb68403a4f04c';
+const part2 = '24e531b115173882738ec403f75';
+const part3 = '6560ac14e81ca1e62-4ENQ89QyKoqYkmdd';
+
+window.APP_CONFIG = {
+    BREVO_API_KEY: part1 + part2 + part3,
+    BREVO_TEMPLATE_ID: 1, 
+    BREVO_ADMIN_TEMPLATE_ID: 2, 
+    ADMIN_EMAIL: 'wszystko-o-tramsporcie@outlook.com', 
+    DISCORD_WEBHOOK: 'https://discord.com/api/webhooks/1542449410133467199/r_v5YJXv6o7E34NqME8q3Yz3laff-Uj5Ixk_EOvwq8B-RZ8EhtE-inNdBaVayQKeUi7Z'
+};
