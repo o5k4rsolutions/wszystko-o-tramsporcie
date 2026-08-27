5c59a901fd4283f462cde16b31e43b58a8a8133a
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-08-27 10:49:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 window.APP_CONFIG = {
     BREVO_API_KEY: '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',
     BREVO_TEMPLATE_ID: 1, 
+    BREVO_ADMIN_TEMPLATE_ID: 2, 
+    ADMIN_EMAIL: 'wszystko-o-tramsporcie@outlook.com', 
     DISCORD_WEBHOOK: 'https://discord.com/api/webhooks/1542449410133467199/r_v5YJXv6o7E34NqME8q3Yz3laff-Uj5Ixk_EOvwq8B-RZ8EhtE-inNdBaVayQKeUi7Z'
 };
