88b1647170814b6b8cc6012cc40bb7e8d28bae12
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-08-27 10:38:40 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+    BREVO_API_KEY: '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',
+    BREVO_TEMPLATE_ID: 1, 
+    DISCORD_WEBHOOK: 'https://discord.com/api/webhooks/1542449410133467199/r_v5YJXv6o7E34NqME8q3Yz3laff-Uj5Ixk_EOvwq8B-RZ8EhtE-inNdBaVayQKeUi7Z'
+};
