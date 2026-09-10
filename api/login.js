189a18223116713e3d2d2a488ed482c4f2ea5556
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-09-10 08:24:40 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   const CLIENT_ID = '1543945334684655686';
   const protocol = req.headers['x-forwarded-proto'] || 'https';
-  const REDIRECT_URI = process.env.REDIRECT_URI || `${protocol}://${req.headers.host}/api/callback`;
+const REDIRECT_URI = 'https://wszystko-o-tramsporcie.vercel.app/api/callback';
   
   const discordOAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20email%20guilds.join`;
   
