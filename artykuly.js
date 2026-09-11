ada87ac6a968aef1e0b8aa848a1e66fce6f1ae1b
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-09-11 11:50:49 +0200

    Update artykuly.js

--- artykuly.js
+++ artykuly.js
@@ -33,7 +33,7 @@ const artykulyBaza = [
     },
     {
         id: 2,
-        tytul: "Uwaga kierowcy i pasażerowie: Weekendowe zamknięcie południowej jezdni ul. Górczewskiej. Zmiany w kursowaniu autobusów",
+        tytul: "WARSZAWA: Uwaga kierowcy i pasażerowie: Weekendowe zamknięcie południowej jezdni ul. Górczewskiej. Zmiany w kursowaniu autobusów",
         kategoria: "Utrudnienia",
         data: "11 września 2026",
         czasCzytania: "5 min czytania",
@@ -66,6 +66,33 @@ const artykulyBaza = [
                 <li><b>Przystanek zastępczy:</b> Uruchomiony zostaje przystanek Os. Górczewska 51, zlokalizowany na ul. Lazurowej (tu za przystankiem Os. Górczewska 01), który posłuży jako krańcowy dla linii 105, 122 i 189.</li>
             </ul>
             <p class="mt-4">Pasażerowie powinni zwrócić uwagę na zaktualizowane rozkłady jazdy obowiązujące w dniach 12–13 września oraz w nocy z 11/12 i 12/13 września. Organizator transportu przeprasza za wszelkie niedogodności i zachęca do wcześniejszego sprawdzania tras w aplikacjach mobilnych oraz na stronie <a href="https://www.wtp.waw.pl" class="text-blue-600 font-semibold hover:underline">WTP</a>.</p>
+        `
+    },
+        {
+        id: 3,
+        tytul: "POZNAŃ: Nowy przystanek „Os. Dębina” dla pasażerów autobusów linii nr 171, 176 i 215",
+        kategoria: "Aktualności",
+        data: "11 września 2026",
+        czasCzytania: "2 min czytania",
+        autor: "O5K4R Solutions",
+        autorAvatar: "content/akt/O5K4R.png",
+        baner: "content/akt/poznan_zmiany_nowa.png",
+        trescPelna: `
+            <p>Zarząd Transportu Miejskiego w Poznaniu poinformował o uruchomieniu nowego punktu zatrzymania dla pojazdów komunikacji miejskiej. Zmiany wejdą w życie w najbliższy poniedziałek, 14 września 2026 roku, i obejmą trzy popularne linie autobusowe: 171, 176 oraz 215.</p>
+            
+            <p>Od poniedziałku pasażerowie korzystający z sieci ZTM w Poznaniu zyskają udogodnienie w rejonie osiedla Dębina. Na ulicy 28 Czerwca 1956 r. oficjalnie zacznie funkcjonować nowy przystanek oznaczony jako Os. Dębina 04.</p>
+
+            <h3 class="font-heading text-xl font-bold text-[#1d1d1f] mt-6 mb-3">Gdzie dokładnie zlokalizowany jest nowy przystanek?</h3>
+            <p>Infrastruktura została ulokowana w dogodnym punkcie – pomiędzy dotychczasową zatoką postojową dla autobusów a łącznikiem usytuowanym przy pobliskiej stacji benzynowej.</p>
+
+            <h3 class="font-heading text-xl font-bold text-[#1d1d1f] mt-6 mb-3">Jak będzie funkcjonował?</h3>
+            <p>Zgodnie z wytycznymi ZTM, nowo otwarty przystanek Os. Dębina 04 będzie pełnił podwójną funkcję:</p>
+         <ul class="list-disc pl-6 space-y-2">
+                <li><b>Przystanku początkowego</b> – dla pojazdów, które rozpoczynają swoje kursy bezpośrednio z pętli Os. Dębina,</li>
+                <li><b>Przystanku przelotowego</b> – dla autobusów poruszających się od strony Lubonia.</li>
+            </ul>
+            <p class="mt-4">Jednocześnie organizator transportu zwrócił uwagę na korektę roli dotychczasowego przystanku. Miejsce oznaczone dotychczas jako <b>Os. Dębina 02</b> od momentu wprowadzenia zmian stanie się w pełni przystankiem przelotowym.
+Nowa inwestycja ma na celu usprawnienie ruchu pasażerskiego w tym rejonie oraz lepsze dopasowanie układu komunikacyjnego do potrzeb osób podróżujących z i w kierunku południowych obrzeży Poznania oraz Lubonia.</p>
         `
     }
 ];
