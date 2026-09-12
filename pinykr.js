d266a3f09e0a45e79bc9fee01d82c3240869266c
o5k4rsolutions <227397329+o5k4rsolutions@users.noreply.github.com>
2026-09-12 10:42:46 +0200

    Create pinykr.js

---
+++ pinykr.js
@@ -0,0 +1,7 @@
+const VALID_PINS = [
+    { email: "klient1@tramsport.pl", pin: "TRAM-1024" },
+    { email: "jan.kowalski@example.com", pin: "TRAM-5582" },
+    { email: "anna.nowak@example.com", pin: "TRAM-9482" },
+    { email: "tramwajoholik@gmail.com", pin: "KM-300-2026" },
+    { email: "test@wszystko-o-tramsporcie.pl", pin: "TEST-PIN-1" }
+];
